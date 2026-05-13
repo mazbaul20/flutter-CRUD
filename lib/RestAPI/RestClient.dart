0f408928ea7676582a8ddf7d230a1d57d3084983
mazbaul20 <mazbaul20@gmail.com>
2026-05-13 13:07:49 +0600

    Delete product

--- lib/RestAPI/RestClient.dart
+++ lib/RestAPI/RestClient.dart
@@ -45,3 +45,23 @@ Future<bool> ProductCreateRequest(FormValues) async {
     return false;
   }
 }
+
+Future<bool> ProductDeleteRequest(id) async {
+  var URL = Uri.parse("http://10.0.2.2:8000/api/v1/products/"+id);
+  var PostHeader = {
+    'Content-Type': 'application/json',
+    "Accept": "application/json",
+  };
+
+  var response = await http.delete(URL, headers: PostHeader);
+  var ResultCode = response.statusCode;
+  var ResultBody = json.decode(response.body);
+
+  if (ResultCode == 200 && ResultBody['status'] == 'success') {
+    SuccessToast("Product Deleted Successfully");
+    return true;
+  } else {
+    ErrorToast("Request fail ! try again");
+    return false;
+  }
+}
--- lib/Screen/ProductGridViewScreen.dart
+++ lib/Screen/ProductGridViewScreen.dart
@@ -22,72 +22,112 @@ class _ProductGridViewScreenState extends State<ProductGridViewScreen> {
   }
 
   CallData() async {
-    Loading=true;
+    Loading = true;
 
     var data = await ProductGridViewListRequest();
     setState(() {
       ProductList = data;
-      Loading=false;
+      Loading = false;
     });
   }
 
+  DeleteItem(id) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete !"),
+          content: Text("Do you want to delete this product?"),
+          actions: [
+            OutlinedButton(
+              onPressed: () async {
+                Navigator.pop(context);
+                setState(() {
+                  Loading = true;
+                });
+                await ProductDeleteRequest(id);
+                CallData();
+              },
+              child: Text("yes"),
+            ),
+
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("No"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Product List"),
-      ),
+      appBar: AppBar(title: Text("Product List")),
       body: Stack(
         children: [
           ScreenBackground(context),
           Container(
-            child: Loading ? Center(child: CircularProgressIndicator()) : RefreshIndicator(
-                child: GridView.builder(
-                        itemCount: ProductList.length,
-                        gridDelegate: ProductGridViewStyle(),
-                        itemBuilder: (context,index){
-                          return Card(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                Expanded(child: Image.network(ProductList[index]['Img'],fit: BoxFit.fill,)),
-                                Container(
-                                  padding: EdgeInsets.fromLTRB(5, 5, 5, 8),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(ProductList[index]['ProductName']),
-                                      SizedBox(height: 10,),
-                                      Text("Price: "+ProductList[index]['UnitPrice']+" BDT"),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          OutlinedButton(onPressed: (){
-
-                                          }, child: Icon(CupertinoIcons.ellipsis_vertical_circle,size: 18,color: colorGreen,)),
-                                          SizedBox(width: 4,),
-                                          OutlinedButton(onPressed: (){
-
-                                          }, child: Icon(CupertinoIcons.delete,size: 18,color: colorRed,)),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
+            child: Loading? Center(child: CircularProgressIndicator()): RefreshIndicator(
+                    child: GridView.builder(
+                      itemCount: ProductList.length,
+                      gridDelegate: ProductGridViewStyle(),
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Image.network(
+                                  ProductList[index]['Img'],
+                                  fit: BoxFit.fill,
                                 ),
-                              ],
-                            ),
-                          );
-                        }
+                              ),
+                              Container(
+                                padding: EdgeInsets.fromLTRB(5, 5, 5, 8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(ProductList[index]['ProductName']),
+                                    SizedBox(height: 10),
+                                    Text("Price: "+ProductList[index]['UnitPrice'] +" BDT"),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        OutlinedButton(
+                                          onPressed: () {},
+                                          child: Icon(CupertinoIcons.ellipsis_vertical_circle,
+                                            size: 18,
+                                            color: colorGreen,
+                                          ),
+                                        ),
+                                        SizedBox(width: 4),
+                                        OutlinedButton(
+                                          onPressed: () {
+                                            DeleteItem(ProductList[index]['_id']);
+                                          },
+                                          child: Icon(CupertinoIcons.delete, size: 18, color: colorRed,),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                onRefresh: () async {
-                  await CallData();
-                }
-            ),
+                    onRefresh: () async {
+                      await CallData();
+                    },
+                  ),
           ),
         ],
       ),
     );
   }
 }
-
